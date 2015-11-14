ccc2090a1487d7f934359d3ae90eeb7105be2053
pavanpn <pavankumar.pn@gmail.com>
2015-11-13 23:03:34 -0800

    Fixed GetData file.

--- getData.py
+++ getData.py
@@ -13,21 +13,16 @@ def main():
 	else:
 		print "SYNTAX: getData <API_KEY> <DOCKET_ID>"
 		quit()
-	results = parseFile.getFilesInDirectory("data/*")
-	print "Num:", results
-
-# Call main
-main()
-
-"""
 	# Get Number of Comments
 	commentData = apiCall.getLastNComments(API_KEY, DOCKET_ID, str(1000), str(22000) )
 	numberofComments = commentData["totalNumRecords"]
-	documents = commentData["documents"]
+	print "Fetching "+numberofComments+" comments."
 	# API limit is 1000. Call API 1+numberOfComments/1000 times
 	numberofIterations = int(numberofComments/1000)+1
 	for index in range (0, numberofIterations, 1):
 		PAGE_OFFSET = index * 1000
 		commentData = apiCall.getLastNComments(API_KEY, DOCKET_ID, str(1000), str(PAGE_OFFSET) )
 		fileIO.writeJsonFile( commentData, "data/file"+str(index) )
-"""
+	print "Done."
+# Call main
+main()
